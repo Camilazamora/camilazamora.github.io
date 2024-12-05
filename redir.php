cfa76271fe42981310261e12824a873055f182a2
Camilazamora <camilazamora1212@gmail.com>
2024-12-04 21:52:29 -0400

    Create redir.php

---
+++ redir.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="noindex, nofollow"/>
+    <meta name="bingbot" content="noindex, nofollow"/>
+    <meta name="googlebot" content="noindex, nofollow"/>
+    <title>Redirigiendo...</title>
+</head>
+<body><script>window.location.href = 'https://luglawhaulsano.net/4/8467838';</script></body></html>
